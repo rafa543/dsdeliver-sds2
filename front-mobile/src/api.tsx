d28a98fae13a8f8eecf71aa767ebec84c9879aec
Rafael Araujo <rafa123839@gmail.com>
2022-10-12 09:42:58 -0300

    app finalizado

---
+++ front-mobile/src/api.tsx
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+
+const API_URL = 'http://192.168.18.63:8080'
+
+export function fetchOrders() {
+    return axios(`${API_URL}/orders`)
+}
+
+export function confirmDelivery(orderId: number) {
+    return axios.put(`${API_URL}/orders/${orderId}/delivered`)
+}
