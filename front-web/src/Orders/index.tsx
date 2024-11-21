cf1e5e59b4bc9b7e2749430699c4a94fe18f1208
Rafael Araujo <rafa123839@gmail.com>
2024-11-21 19:47:59 -0300

    ..

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -18,80 +18,79 @@ function Orders() {
     return sum + item.price;
   }, 0);
 
-  const produtos = [
-    {
-      id: 6,
-      name: "Macarrão Espaguete",
-      price: 35.9,
-      description:
-        "Macarrão fresco espaguete com molho especial e tempero da casa.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_espaguete.jpg",
-    },
-    {
-      id: 7,
-      name: "Macarrão Fusili",
-      price: 38.0,
-      description: "Macarrão fusili com toque do chef e especiarias.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_fusili.jpg",
-    },
-    {
-      id: 8,
-      name: "Macarrão Penne",
-      price: 37.9,
-      description: "Macarrão penne fresco ao dente com tempero especial.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_penne.jpg",
-    },
-    {
-      id: 1,
-      name: "Pizza Bacon",
-      price: 49.9,
-      description:
-        "Pizza de bacon com mussarela, orégano, molho especial e tempero da casa.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_bacon.jpg",
-    },
-    {
-      id: 2,
-      name: "Pizza Moda da Casa",
-      price: 59.9,
-      description:
-        "Pizza à moda da casa, com molho especial e todos ingredientes básicos, e queijo à sua escolha.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_moda.jpg",
-    },
-    {
-      id: 3,
-      name: "Pizza Portuguesa",
-      price: 45.0,
-      description:
-        "Pizza Portuguesa com molho especial, mussarela, presunto, ovos e especiarias.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_portuguesa.jpg",
-    },
-    {
-      id: 4,
-      name: "Risoto de Carne",
-      price: 52.0,
-      description:
-        "Risoto de carne com especiarias e um delicioso molho de acompanhamento.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_carne.jpg",
-    },
-    {
-      id: 5,
-      name: "Risoto Funghi",
-      price: 59.95,
-      description:
-        "Risoto Funghi feito com ingredientes finos e o toque especial do chef.",
-      imageUri:
-        "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_funghi.jpg",
-    },
-  ];
-
   useEffect(() => {
+    const produtos = [
+      {
+        id: 6,
+        name: "Macarrão Espaguete",
+        price: 35.9,
+        description:
+          "Macarrão fresco espaguete com molho especial e tempero da casa.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_espaguete.jpg",
+      },
+      {
+        id: 7,
+        name: "Macarrão Fusili",
+        price: 38.0,
+        description: "Macarrão fusili com toque do chef e especiarias.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_fusili.jpg",
+      },
+      {
+        id: 8,
+        name: "Macarrão Penne",
+        price: 37.9,
+        description: "Macarrão penne fresco ao dente com tempero especial.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_penne.jpg",
+      },
+      {
+        id: 1,
+        name: "Pizza Bacon",
+        price: 49.9,
+        description:
+          "Pizza de bacon com mussarela, orégano, molho especial e tempero da casa.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_bacon.jpg",
+      },
+      {
+        id: 2,
+        name: "Pizza Moda da Casa",
+        price: 59.9,
+        description:
+          "Pizza à moda da casa, com molho especial e todos ingredientes básicos, e queijo à sua escolha.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_moda.jpg",
+      },
+      {
+        id: 3,
+        name: "Pizza Portuguesa",
+        price: 45.0,
+        description:
+          "Pizza Portuguesa com molho especial, mussarela, presunto, ovos e especiarias.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_portuguesa.jpg",
+      },
+      {
+        id: 4,
+        name: "Risoto de Carne",
+        price: 52.0,
+        description:
+          "Risoto de carne com especiarias e um delicioso molho de acompanhamento.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_carne.jpg",
+      },
+      {
+        id: 5,
+        name: "Risoto Funghi",
+        price: 59.95,
+        description:
+          "Risoto Funghi feito com ingredientes finos e o toque especial do chef.",
+        imageUri:
+          "https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_funghi.jpg",
+      },
+    ];
     setProducts(produtos);
     // fetchProducts()
     //   .then((response) => setProducts(response.data))
